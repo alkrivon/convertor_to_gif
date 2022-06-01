b4afa93cff9b83562013ec9f68f7f00168299cc8
Aleksandr Krivonosov <w0rk0n@yandex.ru>
2022-06-01 07:45:23 +0300

    - init commit;

---
+++ src/main/java/ru/alfa_bank/convertor_to_gif/ConvertorToGifApplication.java
@@ -0,0 +1,13 @@
+package ru.alfa_bank.convertor_to_gif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertorToGifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertorToGifApplication.class, args);
+	}
+
+}
